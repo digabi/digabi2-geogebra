32209a6f45e76d5af5bacaf0297e08b705a0517b
Linus Willner <linus.willner@reaktor.com>
2025-07-24 16:54:31 +0300

    Update GeoGebra

---
+++ public/GeoGebra/HTML5/5.0/web/deferredjs/1AD2942F57AF71EEEB93EFC71D07047E/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback2('Xvc(3862,1,htn);_.Hd=function(){};tFm(Qc)(2);\n//# sourceURL=web-2.js\n')
--- public/GeoGebra/HTML5/5.0/web3d/web3d.nocache.mjs
+++ public/GeoGebra/HTML5/5.0/web3d/web3d.nocache.mjs
@@ -104,7 +104,22 @@ window.web3d = function() {
    * code will inject these functions in these placeholders.
    ***************************************************************************/
   // Provides getInstallLocationDoc() function.
-  var frameDoc;
+  // GWT code can be installed anywhere, but an iFrame is the best place if you
+// want both variable isolation and runAsync support. Variable isolation is
+// useful for avoiding conflicts with JavaScript libraries and critical if
+// you want more than one GWT module on your page. The runAsync implementation
+// will need to install additional chunks of code into the same iFrame later.
+//
+// By default, CrossSiteIFrameLinker will use this script to create the iFrame.
+// It may be replaced by overriding CrossSiteIframeLinker.getJsInstallLocation()
+// to return the name of a different resource file. The replacement script may
+// optionally set this variable inside the iframe:
+//
+// $wnd - the location where the bootstrap module is defined. It should also
+//        be the location where the __gwtStatsEvent function is defined.
+//        If not set, the module will set $wnd to window.parent.
+
+var frameDoc;
 
 function getInstallLocationDoc() {
   setupInstallLocation();
@@ -144,6 +159,7 @@ function setupInstallLocation() {
     frameDoc.close();
   }
 }
+
 
   // Installs the script directly, by simply appending a script tag with the
   // src set to the correct location to the install location.
@@ -290,7 +306,7 @@ function setupWaitForBodyLoad(callback) {
       // __PERMUTATIONS_BEGIN__
       // Permutation logic is injected here. this code populates the
       // answers variable.
-      strongName = 'EB0765EC9F3ECD5E6D9038FDE60A0A5F';// __PERMUTATIONS_END__
+      strongName = '9C0A67DAA4A037FD95E6303114DF2E69';// __PERMUTATIONS_END__
       var idx = strongName.indexOf(':');
       if (idx != -1) {
         softPermutationId = parseInt(strongName.substring(idx + 1), 10);
@@ -446,12 +462,13 @@ window.web3d.onReady = function(submodule, userRender) {
         var parentElement = options.element.parentElement;
         previewPositioner.appendChild(options.element);
         parentElement.appendChild(previewPositioner);
-           options.removePreview = function() {
+        options.removePreview = function() {
             var preview = document.querySelector(".ggb_preview");
             if (preview) {
                 preview.parentNode.removeChild(preview);
             }
         }
+        GGBAppletUtils.responsiveResize(parentElement, options);
     }
 
     function updateAppletSettings(settings) {
@@ -489,6 +506,210 @@ window.web3d.onReady = function(submodule, userRender) {
     } else {
         resolve(options);
     }
+
+    var GGBAppletUtils = (function() {
+        "use strict";
+
+        function scaleElement(el, scale) {
+            if (scale != 1) {
+                el.style.transformOrigin = "0% 0% 0px";
+                el.style.webkitTransformOrigin = "0% 0% 0px";
+                el.style.transform = "scale(" + scale + "," + scale + ")";
+                el.style.webkitTransform = "scale(" + scale + "," + scale + ")";
+                el.style.maxWidth = "initial";
+                // Remove the max width from the image and the div
+                if (el.querySelector(".ggb_preview") !== null) {
+                    el.querySelector(".ggb_preview").style.maxWidth = "initial";
+                }
+                if (el.querySelectorAll('.ggb_preview img')[0] !== undefined) {
+                  el.querySelectorAll('.ggb_preview img')[0].style.maxWidth = "initial";
+                }
+                if (el.querySelectorAll('.ggb_preview img')[1] !== undefined) {
+                  el.querySelectorAll('.ggb_preview img')[1].style.maxWidth = "initial"
+                }
+            } else {
+                el.style.transform = "none";
+                el.style.webkitTransform = "none";
+            }
+        }
+
+        function getWidthHeight(appletElem, appletWidth, allowUpscale, autoHeight, noBorder, scaleContainerClass) {
+            // Find the container class
+            var container = null;
+            if (scaleContainerClass != undefined && scaleContainerClass != '') {
+                var parent = appletElem.parentNode;
+                while(parent != null) {
+                    if ((' ' + parent.className + ' ').indexOf(' ' + scaleContainerClass + ' ') > -1) {
+                        container = parent;
+                        break;
+                    } else {
+                        parent = parent.parentNode;
+                    }
+                }
+            }
+
+            var myWidth = 0, myHeight = 0, windowWidth = 0, border = 0, borderRight = 0, borderLeft = 0, borderTop = 0;
+
+            if (container) {
+                myWidth = container.offsetWidth;
+                myHeight = Math.max(autoHeight ? container.offsetWidth : 0, container.offsetHeight);
+            } else {
+                if (window.innerWidth && document.documentElement.clientWidth) {
+                    myWidth = Math.min(window.innerWidth, document.documentElement.clientWidth);
+                    myHeight = Math.min(window.innerHeight, document.documentElement.clientHeight);
+                    // Using mywith instead of innerWidth because after rotating a mobile device the innerWidth is sometimes wrong (e.g. on Galaxy Note III)
+                    // windowWidth = window.innerWidth
+                    windowWidth = myWidth;
+                } else {
+                    //Non-IE
+                    myWidth = window.innerWidth;
+                    myHeight = window.innerHeight;
+                    windowWidth = window.innerWidth;
+                }
+
+                if (appletElem) {
+                    var rect = appletElem.getBoundingClientRect();
+                    if (rect.left > 0) {
+                        if (rect.left <= myWidth && (noBorder === undefined || !noBorder)) {
+                            if (document.dir === 'rtl') {
+                                borderRight = myWidth - rect.width - rect.left;
+                                borderLeft = (windowWidth <= 480 ? 10 : 30);
+                            } else {
+                                borderLeft = rect.left;
+                                borderRight = (windowWidth <= 480 ? 10 : 30);
+                            }
+                            border = borderLeft + borderRight;
+                        }
+                    }
+                }
+            }
+
+            if (appletElem) {
+                if ((allowUpscale === undefined || !allowUpscale) && appletWidth > 0 && appletWidth + border < myWidth) {
+                    myWidth = appletWidth;
+                } else {
+                    myWidth -= border;
+                }
+            }
+
+            return {width: myWidth, height: myHeight};
+        }
+
+        function calcScale(parameters, appletElem, allowUpscale, showPlayButton, scaleContainerClass){
+            if (parameters.isScreenshoGenerator) {
+                return 1;
+            }
+            var ignoreHeight = (showPlayButton !== undefined && showPlayButton);
+            var noScaleMargin = parameters.noScaleMargin != undefined && parameters.noScaleMargin;
+            var valBoolean = function(value) {
+                return (value && value !== "false");
+            };
+            var autoHeight = valBoolean(parameters.autoHeight);
+            var windowSize = getWidthHeight(appletElem, parameters.width, allowUpscale, autoHeight, noScaleMargin, scaleContainerClass);
+            var windowWidth = parseInt(windowSize.width);
+
+            var appletWidth = parameters.width;
+            var appletHeight = parameters.height;
+            if (appletWidth === undefined) {
+                var article = appletElem.querySelector('.appletParameters');
+                if (article) {
+                    appletWidth = article.offsetWidth;
+                    appletHeight = article.offsetHeight;
+                }
+            }
+
+            var xscale = windowWidth / appletWidth;
+            var yscale = (ignoreHeight ? 1 : windowSize.height / appletHeight);
+            if (allowUpscale !== undefined && !allowUpscale) {
+                xscale = Math.min(1, xscale);
+                yscale = Math.min(1, yscale);
+            }
+
+            return Math.min(xscale, yscale);
+        }
+
+        function getScale(parameters, appletElem, showPlayButton) {
+            var scale = 1,
+                autoScale,
+                allowUpscale = false;
+
+            if (parameters.hasOwnProperty('allowUpscale')) {
+                allowUpscale = parameters.allowUpscale;
+            }
+
+            if (parameters.hasOwnProperty('scale')) {
+                scale = parseFloat(parameters.scale);
+                if (isNaN(scale) || scale === null || scale === 0) {
+                    scale = 1;
+                }
+                if (scale > 1) {
+                    allowUpscale = true;
+                }
+            }
+
+            if (!(parameters.hasOwnProperty('disableAutoScale') && parameters.disableAutoScale)) {
+                autoScale = calcScale(parameters, appletElem, allowUpscale, showPlayButton, parameters.scaleContainerClass);
+            } else {
+                return scale;
+            }
+
+            if (allowUpscale && (!parameters.hasOwnProperty('scale') || scale === 1)) {
+                return autoScale;
+            } else {
+                return Math.min(scale, autoScale);
+            }
+        }
+
+        function responsiveResize(appletElem, parameters) {
+            var article = appletElem.querySelector(".ggb_preview");
+
+            if (article) {
+
+                if(article.parentElement && (/fullscreen/).test(article.parentElement.className)){
+                    return; //fullscreen button inside applet pressed
+                }
+
+                var scale = getScale(parameters, appletElem);
+
+                var scaleElem = null;
+                for (var i = 0; i < appletElem.childNodes.length; i++) {
+                    if (appletElem.childNodes[i].className !== undefined && appletElem.childNodes[i].className.match(/^applet_scaler/)) {
+                        scaleElem = appletElem.childNodes[i];
+                        break;
+                    }
+                }
+
+                if (scaleElem !== null && scaleElem.querySelector(".noscale") !== null) {
+                    return;
+                }
+
+                var appName = (parameters.id !== undefined ? parameters.id : "ggbApplet");
+                var app = window[appName];
+
+                if ((app == null || !app.recalculateEnvironments) && scaleElem !== null && !scaleElem.className.match(/fullscreen/)) {
+                    scaleElem.parentNode.style.transform = "";
+                    if (!isNaN(scale) && scale !== 1) {
+                        // Set the scale factor for the applet
+                        scaleElem.parentNode.style.width = (parameters.width * scale) + 'px';
+                        scaleElem.parentNode.style.height = (parameters.height * scale) + 'px';
+                        scaleElement(scaleElem, scale);
+
+                    } else {
+                        // Remove scaling
+                        scaleElement(scaleElem, 1);
+                        scaleElem.parentNode.style.width = (parameters.width) + 'px';
+                        scaleElem.parentNode.style.height = (parameters.height) + 'px';
+                    }
+                }
+            }
+        }
+
+        return {
+            responsiveResize: responsiveResize,
+            getScale: getScale,
+            scaleElement: scaleElement
+        };
+    })();
      });
   }
 
